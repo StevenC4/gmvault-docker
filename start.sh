95878637a58f66bc3dc3877cacc0d1b838418b08
Steven Carroll <stevencarroll90@gmail.com>
2017-08-22 13:17:44 -0600

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d -i -t --name gmvault -v /var/lib/gmvault/conf:/root/.gmvault -v /var/lib/gmvault/database:/root/gmvault-db stevenc4/gmvaultd
+docker run -d -i -t --name gmvault -v /var/lib/gmvault/conf:/root/.gmvault -v /var/lib/gmvault/database:/root/gmvault-db stevenc4/gmvault
